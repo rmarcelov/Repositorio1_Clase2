66d9255630fd44305fd2d47fe52ce7a4485d83f1
rober <rober@PC-Robert>
2025-01-14 21:50:35 -0500

    Carga Inicial

--- src/programas/Ahorroeconomico.java
+++ src/programas/Ahorroeconomico.java
@@ -19,6 +19,7 @@ public class Ahorroeconomico {
      ahorrom=ingreso-gasto;
      ahorroa=ahorrom*12;
      //salida de datos
+     System.out.println("Resultados de la Operacion");
      System.out.println("Ahorro mensual:"+ahorrom);
      System.out.println("Ahorro anual:"+ahorroa);
   }  
